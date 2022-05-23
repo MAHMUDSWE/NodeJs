3db31303db8dfc16f5fc9ae4452fc6c37e24b1ac
Mahmud <mahmudur69@student.sust.edu>
2022-05-24 04:06:44 +0600

    First Server deploy

--- tutorial9/index.js
+++ tutorial9/index.js
@@ -1,28 +1,29 @@
 const fs = require('fs');
 const http = require('http');
-const port = 3000;
+const port = process.env.port;
 const hostName = '127.0.0.1';
 
 const myServer = http.createServer((req, res) =>{
 
-    const handleReadFile = (statusCode, fileLocation) => {
+    const handleReadFile = (statusCode, fileLocation, req) => {
         fs.readFile(fileLocation, (err, data) => {
+            console.log(req.url);
             res.writeHead(statusCode, {"Content-Type": "text/html"});
             res.write(data);
             res.end();
         });
     }
     if(req.url === '/'){
-        handleReadFile(200, "./views/index.html");
+        handleReadFile(200, "./views/index.html", req);
     }
     else if(req.url === '/about'){
-        handleReadFile(200, "./views/about.html");
+        handleReadFile(200, "./views/about.html", req);
     }
     else if(req.url === '/contact'){
-        handleReadFile(200, "./views/contact.html")
+        handleReadFile(200, "./views/contact.html", req)
     }
     else{
-        handleReadFile(400, "./views/error.html");
+        handleReadFile(400, "./views/error.html", req);
     }
 });
 
